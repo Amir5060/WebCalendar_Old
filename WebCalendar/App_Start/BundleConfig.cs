f0df1924db889e72966beb14e27b2e4e52caf171
Amir <a_5060@yahoo.com>
2017-06-08 13:30:13 +0300

    Calendar works for the first time!

--- WebCalendar/App_Start/BundleConfig.cs
+++ WebCalendar/App_Start/BundleConfig.cs
@@ -26,6 +26,14 @@ namespace WebCalendar
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/fullcalendarcss").Include(
+                "~/Content/fullcalendar.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/fullcalendarjs").Include(
+                "~/Scripts/fullcalendar/fullcalendar.min.js",
+                "~/Scripts/fullcalendar/fullcalendar.js",
+                "~/Scripts/moment.js"));
         }
     }
 }
