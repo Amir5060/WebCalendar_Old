59c451f930b2984db6d95ad729e2f57516ce08f0
Amir <a_5060@yahoo.com>
2017-06-09 13:35:08 +0300

    SignalR works in client side!

---
+++ WebCalendar/Hubs/CalendarHub.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebCalendar.Hubs
+{
+    public class CalendarHub: Hub
+    {
+        static ConcurrentDictionary<string, string> dic = new ConcurrentDictionary<string, string>();
+
+        public void Send(string userId, string name, DateTime startDate, DateTime endDate)
+        {
+            CalendarClass myCalendarClass = new CalendarClass();
+            Clients.All.broadCastMessage(name, startDate, endDate);
+        }
+
+        public void NewUser(string guid, string name)
+        {
+            dic.TryAdd(guid, name);
+            //Clients.Others.enters(guid);
+        }
+    }
+
+    
+
+    public class CalendarClass
+    {
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<Guid> UserId { get; set; }
+    }
+}
--- WebCalendar/Startup.cs
+++ WebCalendar/Startup.cs
@@ -9,6 +9,7 @@ namespace WebCalendar
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            app.MapSignalR();
         }
     }
 }
