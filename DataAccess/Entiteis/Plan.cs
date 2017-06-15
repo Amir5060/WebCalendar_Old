73ce7f5f57f3fef7cd0a5032f79eae8862114bdc
Naji <Amir.Naji@kavokerr.com>
2017-06-15 09:37:50 +0300

    Add DataAccess

---
+++ DataAccess/Entiteis/Plan.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entiteis
+{
+    public class Plan
+    {
+        [Key]
+        public int CalendarId { get; set; }
+        [Required]
+        [Display(Name = "Title")]
+        public string CalendarTitle { get; set; }
+        [Display(Name = "Description")]
+        public string CalendarDescription { get; set; }
+        [Required]
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+        [Required]
+        [Display(Name = "End Date")]
+        public DateTime EndDate { get; set; }
+        [Required]
+        public DateTime CreatedPlan { get; set; }
+        [Required]
+        public DateTime ModifiedPlan { get; set; }
+        [Required]
+        [Display(Name = "Plan Color")]
+        public string PlanColor { get; set; }
+        [Required]
+        public string UserId { get; set; }
+    }
+}
